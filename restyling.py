a675532584d1f4a2ce66e159285b91f339c292b8
Barba02 <69804640+Barba02@users.noreply.github.com>
2022-02-28 17:25:14 +0100

    Add files via upload

--- restyling.py
+++ restyling.py
@@ -12,13 +12,19 @@ if __name__ == "__main__":
     if not os.path.exists(file_blif):
         print("File non trovato")
         exit(1)
-    # tentativi da eseguire
+    # input e verifica tentativi da eseguire
     num_tentativi = int(sys.argv[2])
     if num_tentativi < 1:
         print("Inserire almeno 1 tentativo")
         exit(1)
-    # tempo di esecuzione in secondi
+    # input e verifica tempo di esecuzione in secondi
     tempo_esecuzione = int(sys.argv[3])
     if tempo_esecuzione < 1:
         print("Inserire un tempo di esecuzione di almeno 1 secondo")
-
+        exit(1)
+    # input e verifica modalità di minimizzazione
+    mode = str(sys.argv[4])
+    if mode != "a" and mode != "r" and mode != "ar":
+        print("Inserire una modalità di minimizzazione valida")
+        exit(1)
+    # thread
